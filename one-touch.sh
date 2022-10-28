2fb6ede50419f32da484f8e5118161666968bc57
xiaohunet <56124396+xiaohunet@users.noreply.github.com>
2022-10-29 04:43:49 +0800

    Update one-touch.sh

--- one-touch.sh
+++ one-touch.sh
@@ -5,6 +5,28 @@ WORKSPACE=/opt/ServerStatus
 mkdir -p ${WORKSPACE}
 cd ${WORKSPACE}
 
+wget https://humdi.net/vnstat/vnstat-latest.tar.gz
+tar zxvf vnstat-latest.tar.gz
+cd vnstat-*
+./configure --prefix=/usr --sysconfdir=/etc && make && make install
+vnstatd -d
+cp -v examples/systemd/vnstat.service /etc/systemd/system/
+systemctl enable vnstat
+systemctl start vnstat
+
+#初始化文件
+
+# Debian
+cp -v examples/init.d/debian/vnstat /etc/init.d/
+update-rc.d vnstat defaults
+service vnstat start
+
+# 红帽/CentOS
+cp -v examples/init.d/redhat/vnstat /etc/init.d/
+chkconfig vnstat on
+service vnstat start
+
+cd ${WORKSPACE}
 # 下载, arm 机器替换 x86_64 为 aarch64
 OS_ARCH="x86_64"
 latest_version=$(curl -m 10 -sL "https://api.github.com/repos/zdz/ServerStatus-Rust/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
@@ -15,25 +37,6 @@ wget --no-check-certificate -qO "client-${OS_ARCH}-unknown-linux-musl.zip"  "htt
 unzip -o "server-${OS_ARCH}-unknown-linux-musl.zip"
 unzip -o "client-${OS_ARCH}-unknown-linux-musl.zip"
 
-
-# 在client端安装 vnstat
-## Centos
-sudo yum install epel-release -y
-sudo yum install -y vnstat
-## Ubuntu/Debian
-sudo apt install -y vnstat
-
-# 修改 /etc/vnstat.conf
-# BandwidthDetection 0
-# MaxBandwidth 0
-# 默认不是 eth0 网口的需要置空 Interface 来自动选择网口
-# 没报错一般不需要改
-# Interface ""
-# 重启网络服务
-systemctl restart vnstat
-# 开机自动启动
-systemctl enable vnstat
-
 # systemd service
 mv -v stat_server.service /etc/systemd/system/stat_server.service
 mv -v stat_client.service /etc/systemd/system/stat_client.service
@@ -41,11 +44,11 @@ mv -v stat_client.service /etc/systemd/system/stat_client.service
 systemctl daemon-reload
 
 # 启动
-systemctl start stat_server
+# systemctl start stat_server
 systemctl start stat_client
 
 # 状态查看
-systemctl status stat_server
+# systemctl status stat_server
 systemctl status stat_client
 
 # 使用以下命令开机自启
